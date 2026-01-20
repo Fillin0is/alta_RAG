3d219c018cdb4e789b616e60d52c4ec44ea505d6
Fillin0is <kopylovnikita1228@gmail.com>
2026-01-20 11:41:47 +0300

    exception installing models

--- api/install.py
+++ api/install.py
@@ -6,18 +6,25 @@ from config import EMBED_MODEL_PATH, LLM_MODEL_PATH
 
 def install_embed_model():
     '''Download embedding model for vectorization query'''
-    model = SentenceTransformer("intfloat/multilingual-e5-base")
-    model.save(str(EMBED_MODEL_PATH))
-    print(f'Модель скачана в {str(EMBED_MODEL_PATH)}')
+    try:
+        model = SentenceTransformer("intfloat/multilingual-e5-base")
+        model.save(str(EMBED_MODEL_PATH))
+        print(f'Модель скачана в {str(EMBED_MODEL_PATH)}')
+    except Exception as e:
+        print(e)
+
 
 def install_LLM_model():
     '''Download LLM model for generate response for client'''
-    hf_hub_download(
-        repo_id="MaziyarPanahi/Mistral-7B-Instruct-v0.3-GGUF",
-        filename="Mistral-7B-Instruct-v0.3.Q8_0.gguf",
-        local_dir="./models"
-    )
-    print(f'Модель скачана в {str(LLM_MODEL_PATH)}')
+    try:
+        hf_hub_download(
+            repo_id="MaziyarPanahi/Mistral-7B-Instruct-v0.3-GGUF",
+            filename="Mistral-7B-Instruct-v0.3.Q8_0.gguf",
+            local_dir="./models"
+        )
+        print(f'Модель скачана в {str(LLM_MODEL_PATH)}')
+    except Exception as e:
+        print(e)
 
 if __name__ == '__main__':
     install_embed_model()
